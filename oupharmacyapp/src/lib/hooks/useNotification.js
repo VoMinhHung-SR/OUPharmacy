80e017160c77c2dfca3617c39d5ffc909b20c54b
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-03-04 22:39:21 +0700

    refactor: refactor custome hook useNotification

--- oupharmacyapp/src/lib/hooks/useNotification.js
+++ oupharmacyapp/src/lib/hooks/useNotification.js
@@ -1,36 +1,41 @@
 import { collection, doc, getDocs, orderBy, query, where } from "firebase/firestore"
-import { useContext, useEffect, useState } from "react"
+import { useMemo } from "react"
+import { useCallback, useContext, useEffect, useState } from "react"
 import { useCollection } from "react-firebase-hooks/firestore"
 import { userContext } from "../../App"
 import { db } from "../../config/firebase"
 import { generateQueryGetNotification } from "../utils/getRecipientNotification"
 
 const useNotification = () => {
-    const [user]=useContext(userContext)
-    const queryGetNotification = generateQueryGetNotification(10)
+    const [user, userReady ]=useContext(userContext)
     const [isLoading, setIsLoading] = useState(true)
-    // const [notificationsSnapshot, notificationsLoading, __error] = useCollection(queryGetNotification)
+    const queryGetNotification = generateQueryGetNotification(user?.id)
+    const [notificationsSnapshot, notificationsLoading, __error] = useCollection(queryGetNotification)
     const [notifyListContent, setNotifyListContent] = useState([])
-    useEffect(() => {
-        const getNotifications = async (recipientId) => {
-            const queryNotifications = query(
-                collection(db, 'notifications'),
-                where('recipient_id', '==', recipientId),
-                orderBy('sent_at', 'desc')
-            );
-            const notifyListSnapshot = await getDocs(queryNotifications);
-            // notifyListSnapshot.docs.map will return a single-line data
-            setNotifyListContent(notifyListSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-            setIsLoading(false);
-        };
-        
-        if (user) {
-            getNotifications(user.id);
+    if(!userReady)
+        return {
+            notifyListContent,
+            isLoading: true
         }
-    }, [user?.id]);
+    // if snapshot have data then map to state
+    useEffect(()=> {
+        try{
+            if(!notificationsLoading){
+                setNotifyListContent(notificationsSnapshot?.docs?.map((doc) => ({ id: doc.id, ...doc.data() })));
+            }else {
+                if(!notificationsLoading &&notificationsSnapshot?.size === 0)
+                    setNotifyListContent([])
+            }
+        }catch(err){
+            console.log(err)
+        }
+        
+    }, [notificationsSnapshot])
+
     return {
+        notificationsSnapshot: notificationsSnapshot?.docs,
         notifyListContent,
-        isLoading
+        isLoading: notificationsLoading,
     }
 }
 export default useNotification
