8063b81d11906cc194bcbae7db466bdce82cbf26
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-03-06 23:22:17 +0700

    config: add i18n for notification messages

--- oupharmacyapp/src/modules/pages/NotificationComponents/NotifyMessage/index.jsx
+++ oupharmacyapp/src/modules/pages/NotificationComponents/NotifyMessage/index.jsx
@@ -1,16 +1,26 @@
-import { Avatar, Box, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import Loading from "../../../common/components/Loading"
+import { Avatar, Box, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Typography } from "@mui/material"
+import { useTranslation } from "react-i18next";
+import { STATUS_BOOKING_CONFIRMED, STATUS_BOOKING_DONE } from "../../../../lib/constants";
 
 const NotifyMessage = ({content, recipientId, examinationId, sentAt, avatar}) => {
+    const {t} = useTranslation(['common'])
     return (
         <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
                 <Avatar sx={{ width: 32, height: 32 }} src={avatar ? avatar : 'None' }/>
-                    {/* {examinationId} */}
-                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
               </ListItemIcon>
-              <ListItemText primary={content} />
+      
+              <ListItemText >
+              <Typography component="div" className="ou-list-item-text-container " style={{ maxHeight: '70px'}}>
+                {content === STATUS_BOOKING_CONFIRMED ? t('common:notifyExaminationConfirm') : content === STATUS_BOOKING_DONE ? "DONE" : <></>}
+              </Typography>
+              </ListItemText>
+                {/* // className="ou-break-words "
+                // primary={
+                //   content === STATUS_BOOKING_CONFIRMED ? (<Typography> {t('common:notifyExaminationConfirm')}</Typography> ):
+                //   content === STATUS_BOOKING_DONE ? "DONE" : <></>
+                // } /> */}
             </ListItemButton>
           </ListItem>
     )
