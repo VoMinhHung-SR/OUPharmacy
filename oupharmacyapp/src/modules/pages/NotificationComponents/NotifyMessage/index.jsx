6a130aecbdb545040dce856989d613fffab6e382
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-03-09 23:41:59 +0700

    refactor: hot fix moment notifications message

--- oupharmacyapp/src/modules/pages/NotificationComponents/NotifyMessage/index.jsx
+++ oupharmacyapp/src/modules/pages/NotificationComponents/NotifyMessage/index.jsx
@@ -1,9 +1,12 @@
 import { Avatar, Box, ListItem, ListItemButton, ListItemIcon, ListItemText, TextField, Typography } from "@mui/material"
+import moment from "moment";
 import { useTranslation } from "react-i18next";
 import { STATUS_BOOKING_CONFIRMED, STATUS_BOOKING_DONE } from "../../../../lib/constants";
+import { convertFirestoreTimestampToString } from "../../../../lib/utils/getMessagesInConversation";
 
 const NotifyMessage = ({content, recipientId, examinationId, sentAt, avatar}) => {
     const {t} = useTranslation(['common'])
+    console.log(convertFirestoreTimestampToString(sentAt))
     return (
         <ListItem disablePadding>
             <ListItemButton>
@@ -14,6 +17,11 @@ const NotifyMessage = ({content, recipientId, examinationId, sentAt, avatar}) =>
               <ListItemText >
               <Typography component="div" className="ou-list-item-text-container " style={{ maxHeight: '70px'}}>
                 {content === STATUS_BOOKING_CONFIRMED ? t('common:notifyExaminationConfirm') : content === STATUS_BOOKING_DONE ? "DONE" : <></>}
+              </Typography>
+              <Typography component="div" style={{ maxHeight: '70px'}}>
+                <span className="ou-text-[12px]" >
+                  {sentAt ? moment(convertFirestoreTimestampToString(sentAt)).fromNow()  : <></>}
+                </span>
               </Typography>
               </ListItemText>
                 {/* // className="ou-break-words "
