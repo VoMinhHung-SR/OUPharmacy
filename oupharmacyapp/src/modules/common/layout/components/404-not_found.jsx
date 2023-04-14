642478f4f542bd3845500304e774741e9e1359ef
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-04-14 23:55:13 +0700

    config: add some svg error routing and i18n home

--- oupharmacyapp/src/modules/common/layout/components/404-not_found.jsx
+++ oupharmacyapp/src/modules/common/layout/components/404-not_found.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next"
 import { useNavigate } from "react-router";
 import Loading from "../../components/Loading";
+import Icon404PageNotFound from "../../../../lib/assets/icon404PageNotFound";
 
 const NotFound = () => {
     const {t, ready} = useTranslation('common');
@@ -20,7 +21,8 @@ const NotFound = () => {
                 ou-flex-col ou-flex ou-justify-center ou-items-center
                 ou-top-0 ou-bottom-0 ou-w-full ou-place-items-center'>
                 <Container className="ou-text-center ou-mt-5">
-                    <h3 className="ou-text-red-600 ou-text-xl">{t('common:errNotFound')}</h3>
+                    <Box className="ou-flex ou-items-center ou-justify-center ou-m-auto"><Icon404PageNotFound/></Box>
+                    <h3 className="ou-text-red-600 ou-text-xl ou-mt-3">{t('common:errNotFound')}</h3>
                     <Typography className='text-center'>
                         <h3>{t('common:backToHomepage')}</h3>
                         <Button onClick={() => { router('/') }}>{t('here')}!</Button>
--- oupharmacyapp/src/modules/common/layout/userRoute.jsx
+++ oupharmacyapp/src/modules/common/layout/userRoute.jsx
@@ -5,6 +5,7 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import { userContext } from '../../../App';
 import useConversationList from '../../pages/ConversationListComponents/hooks/useConversationList';
 import Loading from '../components/Loading';
+import Icon401Unauthorized from '../../../lib/assets/icon401Unauthorized';
 
 const ProtectedUserRoute = () => {
     const {t, ready} = useTranslation(['common','modal']);
@@ -28,7 +29,8 @@ const ProtectedUserRoute = () => {
                     <Box className='ou-absolute ou-p-5 ou-text-center 
                         ou-flex-col ou-flex ou-justify-center ou-items-center
                         ou-top-0 ou-bottom-0 ou-w-full ou-place-items-center'>
-                        <Container className="ou-text-center ou-mt-5">
+                        <Container className="ou-text-center">
+                            <Box className="ou-flex ou-items-center ou-justify-center"><Icon401Unauthorized /></Box>
                             <h4 className='ou-text-red-600 ou-text-xl'>{t('common:errNullUser')}</h4>
                             <Button onClick={() => { router('/login') }}>{t('common:here')}!</Button>
                         </Container>
