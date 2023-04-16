bb4798a82070c513a1ecbc4937e3eeb734c51dd1
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-04-16 08:59:15 +0700

    feat: refactor UI homepage and add responsive

--- oupharmacyapp/src/cron/job/at_midnight.js
+++ oupharmacyapp/src/cron/job/at_midnight.js
@@ -6,7 +6,7 @@ import { endpoints } from "../../config/APIs";
 
 export const jobMidnight = () => {
     // [  0 * 0-5 * * * ] : every one hour in 0-5 AM
-    const job = new CronJob('02 07 * * *', () => {
+    const job = new CronJob('0 0-7 * * *', () => {
         fetch(BACKEND_BASEURL + endpoints['get-list-exam-today'])
           .then((response) => response.json())
           .then((data) => {
---
+++ oupharmacyapp/src/lib/assets/iconFaceBook.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+const IconFaceBook = (props) => (
+  <svg
+    fill={props.color}
+    width={props.width ? props.width : "32px"}
+    height={props.height ? props.height : "32px"}
+    viewBox="0 0 0.96 0.96"
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="Layer 1"
+    {...props}
+  >
+    <path d="M0.605 0.213H0.68V0.086A1.044 1.044 0 0 0 0.57 0.08c-0.109 0 -0.183 0.066 -0.183 0.188v0.105H0.264v0.142h0.123V0.88h0.147v-0.365h0.122l0.018 -0.142h-0.141V0.282c0 -0.042 0.011 -0.069 0.07 -0.069Z" />
+  </svg>
+);
+export default IconFaceBook;
