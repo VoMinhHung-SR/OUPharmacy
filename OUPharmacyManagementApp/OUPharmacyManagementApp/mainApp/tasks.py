5b608e6ed1e92f510d96e6c33f18a392858c619d
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-05-09 23:43:20 +0700

    refator: add new server redis to BE

--- OUPharmacyManagementApp/OUPharmacyManagementApp/mainApp/tasks.py
+++ OUPharmacyManagementApp/OUPharmacyManagementApp/mainApp/tasks.py
@@ -4,4 +4,5 @@ from celery import shared_task
 # Run task: celery -A OUPharmacyManagementApp.celery worker --pool=solo --loglevel=info
 @shared_task
 def send_remind_email():
+    print("Lich nhac nho tai kham hom nay")
     return "Lich nhac nho tai kham"
