0d890e680aa7362729aa02343a1d3c415568ce1f
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-05-28 23:46:40 +0700

    refactor: re-active  chat bot

--- oupharmacyapp/src/chatbot/index.jsx
+++ oupharmacyapp/src/chatbot/index.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 
 export const OUPharmacyChatBot = () => {
   React.useEffect(() => {
-    (function (d, m) {
-      var kommunicateSettings = {
-        appId: '2aa5fb4081ecebd15ce8f31b534ae461',
-        popupWidget: true,
-        automaticChatOpenOnNavigation: true,
+    // (function (d, m) {
+    //   var kommunicateSettings = {
+    //     appId: '2aa5fb4081ecebd15ce8f31b534ae461',
+    //     popupWidget: true,
+    //     automaticChatOpenOnNavigation: true,
+    //   };
+    //   var s = document.createElement('script');
+    //   s.type = 'text/javascript';
+    //   s.async = true;
+    //   s.src = 'https://widget.kommunicate.io/v2/kommunicate.app';
+    //   var h = document.getElementsByTagName('head')[0];
+    //   h.appendChild(s);
+    //   window.kommunicate = m;
+    //   m._globals = kommunicateSettings;
+    // })(document, window.kommunicate || {});
+
+    (function(d, m){
+        var kommunicateSettings = {
+          appId:'34ecb68da4ad3dc79330e73674c66e979',
+          popupWidget:true,
+          automaticChatOpenOnNavigation:true
       };
-      var s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = 'https://widget.kommunicate.io/v2/kommunicate.app';
-      var h = document.getElementsByTagName('head')[0];
-      h.appendChild(s);
-      window.kommunicate = m;
-      m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
+        var s = document.createElement("script"); 
+        s.type = "text/javascript"; 
+        s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; 
+        h.appendChild(s);
+        window.kommunicate = m; 
+        m._globals = kommunicateSettings;
+      })(document, window.kommunicate || {});
   }, []);
 
   return <div></div>;
