ed6b1cf295836d529390963498a14e06388325b9
VoMinhHung-SR <vominhhung1546@gmail.com>
2023-06-07 23:40:21 +0700

    refactor: add new logic add booking

--- oupharmacyapp/src/modules/pages/BookingComponents/DoctorAvailabilityTime/index.jsx
+++ oupharmacyapp/src/modules/pages/BookingComponents/DoctorAvailabilityTime/index.jsx
@@ -1,35 +1,57 @@
-const DoctorAvailabilityTime = ({ disabledTimes, isLoading, onChange }) => {
-    const renderRadioButtons = () => {
-      const radioButtons = [];
-      for (let hour = 7; hour <= 16; hour++) {
-        const label = `${hour}:00 - ${hour + 1}:00`;
-        const isDisabled = disabledTimes.some(
-          (time) =>
-            time.start_time === `${hour.toString().padStart(2, "0")}:00:00` &&
-            time.end_time === `${(hour + 1).toString().padStart(2, "0")}:00:00`
-        );
-        radioButtons.push(
-          <label key={hour} className={`ou-radio-label ${isDisabled ? 'ou-opacity-50 ou-cursor-not-allowed' : ''}`}>
-            <input
-              type="radio"
-              name="hour"
-              value={label}
-              className="ou-radio-input"
-              disabled={isDisabled}
-              onChange={onChange}
-            />
-            <div className="ou-radio-custom">{label}</div>
-          </label>
-        );
-      }
-      return radioButtons;
+import React, { useState } from 'react';
+
+const DoctorAvailabilityTime = ({ disabledTimes, isLoading, selectedStartTime, selectedEndTime, onChange }) => {
+  const [selectedTime, setSelectedTime] = useState({ start: selectedStartTime, end: selectedEndTime });
+
+  const renderRadioButtons = () => {
+    return Array.from({ length: 10 }, (_, index) => {
+      const startHour = `${(7 + index).toString().padStart(2, "0")}:00:00`;
+      const endHour = `${(8 + index).toString().padStart(2, "0")}:00:00`;
+      const label = `${(7 + index).toString().padStart(2, "0")}:00 - ${(8 + index).toString().padStart(2, "0")}:00`;
+      const isDisabled = disabledTimes.some(
+        (time) => time.start_time === startHour && time.end_time === endHour
+      );
+      const isSelected = startHour === selectedTime.start && endHour === selectedTime.end;
+      const shouldDisable = isDisabled && (!isSelected || !selectedStartTime || !selectedEndTime);
+
+      return (
+        <label
+          key={index}
+          className={`ou-radio-label ${shouldDisable ? 'ou-opacity-50 ou-cursor-not-allowed' : ''} ${isSelected ? 'ou-selected' : ''}`}
+        >
+          <input
+            type="radio"
+            name="hour"
+            value={label}
+            className="ou-radio-input"
+            disabled={shouldDisable}
+            onChange={handleChange}
+            checked={isSelected}
+          />
+          <div className={`ou-radio-custom ${isSelected ? 'ou-selected-time' : ''}`}>{label}</div>
+        </label>
+      );
+    });
+  };
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const [start, end] = value.split(' - ').map((time) => time.trim());
+  
+    const formatTime = (time) => {
+      const [hours, minutes] = time.split(':');
+      return `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}:00`;
     };
   
-    return (
-      <div className="ou-radio-container">
-        {!isLoading && renderRadioButtons()}
-      </div>
-    );
+    setSelectedTime({ start: formatTime(start), end: formatTime(end) });
+    onChange(event);
   };
-  
-  export default DoctorAvailabilityTime;
+
+  return (
+    <div className="ou-radio-container">
+      {!isLoading && renderRadioButtons()}
+    </div>
+  );
+};
+
+export default DoctorAvailabilityTime;
--- oupharmacyapp/src/pages/profile/examinations/index.jsx
+++ oupharmacyapp/src/pages/profile/examinations/index.jsx
@@ -7,11 +7,15 @@ import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CustomModal from "../../../modules/common/components/Modal";
+import ExaminationUpdate from "../../../modules/pages/ExaminationListComponents/ExaminationUpdate";
+import useCustomModal from "../../../lib/hooks/useCustomModal";
+import CustomCollapseListItemButton from "../../../modules/common/components/collapse/ListItemButton";
 const ExaminationList = () =>{
     const { isLoading, examinationList, handleDeleteExamination, 
         handleChangePage, page,pagination} = useExaminationList();
     const router = useNavigate();
-
+ 
     const {t,ready} = useTranslation(['examinations','common'])   
     
     if(!ready)
@@ -64,64 +68,8 @@ const ExaminationList = () =>{
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    {examinationList.map(e => (
-                                        <TableRow
-                                            key={e.id}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <TableCell component="th" scope="row" >
-                                                <Typography>
-                                                    {e.id}
-                                                </Typography>
-                                            </TableCell>
-
-                                            <TableCell align="center">
-                                                <Typography className="ou-table-truncate-text-container">
-                                                    {e.description}
-                                                </Typography>
-                                            </TableCell>
-                                            <TableCell align="center">
-                                                <Typography>{moment(e.created_date).format('DD/MM/YYYY')}</Typography>
-                                            </TableCell>
-                                            <TableCell align="center">{e.mail_status === true ? t('sent') : t('noSent')}</TableCell>
-                                            <TableCell align="center">
-                                                <Typography>
-                                                    {e.patient.first_name +" "+ e.patient.last_name}
-                                                </Typography>
-                                            </TableCell>
-                                            <TableCell align="center">
-                                                <Box  className="!ou-flex ou-justify-center ou-items-center">
-                                                    {!e.mail_status  &&  
-                                                    <Tooltip title={t('common:edit')} followCursor className="hover:ou-cursor-pointer ">
-                                                    {/* <span> */}
-                                                        <Button variant="contained"
-                                                                className="!ou-mr-2 !ou-min-w-[68px]  !ou-p-2  hover:ou-cursor-pointer"
-                                                                color="success" >
-                                                                <EditIcon></EditIcon>
-                                                        </Button>
-                                                    {/* </span> */}
-                                                </Tooltip>
-                                                }
-                                                    <Tooltip title={t('common:delete')} followCursor className="hover:ou-cursor-pointer">
-                                                        <span>
-                                                        <Button 
-                                                            className="!ou-min-w-[68px]  !ou-p-2 hover:ou-cursor-pointer"
-                                                                variant="contained"
-                                                                onClick={()=>{
-                                                                    handleDeleteExamination(e.id)
-                                                                }}
-                                                                color="error" >
-                                                                    <DeleteIcon></DeleteIcon>
-                                
-                                                            </Button>
-                                                        </span>
-                                                    </Tooltip>
-
-                                                </Box>
-                                                
-                                           
-                                            </TableCell>
-                                        </TableRow>
+                                    {examinationList.map(examination => (
+                                        <OwnerExaminationUpdate e={examination}   handleDeleteExamination={() => handleDeleteExamination(examination.id)}/>
                                     ))}
                                 </TableBody>
                             </Table>
@@ -143,6 +91,100 @@ const ExaminationList = () =>{
                  
                 )
         }
+        
     </>)
 } 
 export default ExaminationList
+
+
+export const OwnerExaminationUpdate = ({e, handleDeleteExamination}) => {
+    const {t,ready} = useTranslation(['examinations','common'])  
+    const { handleCloseModal, isOpen, handleOpenModal } = useCustomModal(); 
+    return (
+        <>
+           <TableRow
+        key={e.id}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    >
+        <TableCell component="th" scope="row" >
+            <Typography>
+                {e.id}
+            </Typography>
+        </TableCell>
+
+        <TableCell align="center">
+            <Typography className="ou-table-truncate-text-container">
+                {e.description}
+            </Typography>
+        </TableCell>
+        <TableCell align="center">
+            <Typography>{moment(e.created_date).format('DD/MM/YYYY')}</Typography>
+        </TableCell>
+        <TableCell align="center">{e.mail_status === true ? t('sent') : t('noSent')}</TableCell>
+        <TableCell align="center">
+            <Typography>
+                {e.patient.first_name +" "+ e.patient.last_name}
+            </Typography>
+        </TableCell>
+        <TableCell align="center">
+            <Box  className="!ou-flex ou-justify-center ou-items-center">
+                {!e.mail_status  &&  
+                <Tooltip title={t('common:edit')} followCursor className="hover:ou-cursor-pointer ">
+                {/* <span> */}
+                    <Button variant="contained"
+                            className="!ou-mr-2 !ou-min-w-[68px]  !ou-p-2  hover:ou-cursor-pointer"
+                            color="success"
+                            onClick={handleOpenModal}
+                            >
+                            <EditIcon></EditIcon>
+                    </Button>
+                {/* </span> */}
+            </Tooltip>
+            }
+                <Tooltip title={t('common:delete')} followCursor className="hover:ou-cursor-pointer">
+                    <span>
+                    <Button 
+                        className="!ou-min-w-[68px]  !ou-p-2 hover:ou-cursor-pointer"
+                            variant="contained"
+                            onClick={()=> handleDeleteExamination()}
+                            color="error" >
+                                <DeleteIcon></DeleteIcon>
+
+                        </Button>
+                    </span>
+                </Tooltip>
+
+            </Box>
+            
+       
+        </TableCell>
+    </TableRow>
+
+    <CustomModal
+            title={t('medicalRecords')}
+            className="ou-w-[900px] ou-text-center"
+            open={isOpen}
+            onClose={handleCloseModal}
+            content={<Box>
+                  {/* <CustomCollapseListItemButton title={t("diagnose")} 
+                  content={
+                    <ExaminationUpdate examination={e} />
+                  }
+                />
+                <CustomCollapseListItemButton title={t("patientInfo")}
+                   content={
+                    <ExaminationUpdate examination={e} />
+                  }
+                /> */}
+                  <ExaminationUpdate examination={e} handleClose={handleCloseModal}/>
+            </Box>}
+            actions={[
+            <Button key="cancel" onClick={handleCloseModal}>
+                {t('modal:cancel')}
+            </Button>
+            ]}
+            />
+        </>
+     
+    )
+}
